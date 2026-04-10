d80c36124c57ca3bdcd59fc7b28748758311c1ed
rahulinfolabz <rahulinfolabz@gmail.com>
2026-04-10 14:39:51 +0530

    booking id update

--- apis/user/BookService.js
+++ apis/user/BookService.js
@@ -20,7 +20,7 @@ async function BookService(req, res) {
       return res.status(404).json({ success: false, message: "Service not found" });
     }
 
-    await db.collection("bookings").insertOne({
+    let booking = await db.collection("bookings").insertOne({
       user_id: new ObjectId(req.user._id),
       service_id: new ObjectId(service_id),
       booking_datetime: new Date(booking_datetime),
@@ -32,7 +32,7 @@ async function BookService(req, res) {
       created_at: new Date(),
     });
 
-    return res.status(201).json({ success: true, message: "Service booked successfully" });
+    return res.status(201).json({ success: true, message: "Service booked successfully", booking_id: booking.insertedId });
   } catch (error) {
     console.error("BookService.js: ", error);
     return res.status(500).json({ success: false, message: "Internal server error" });
